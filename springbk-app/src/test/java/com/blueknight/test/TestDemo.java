238f3f33c6a0e21307266c327899e5512c01ee92
liuyang <494821083@qq.com>
2017-01-11 10:18:34 +0800

    nexus

---
+++ springbk-app/src/test/java/com/blueknight/test/TestDemo.java
@@ -0,0 +1,30 @@
+package com.blueknight.test;
+
+import com.blueknight.dao.po.User;
+import com.blueknight.mapper.UserMapper;
+import com.blueknight.utils.BKUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by liuyang on 2017/1/11.
+ */
+
+@ContextConfiguration("classpath*:config/spring/applicationContext-*.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestDemo {
+    @Autowired
+    private UserMapper userMapper;
+    @Test
+    public void test(){
+        BKUtil.hello();
+        List<User> list = userMapper.select();
+        System.out.println(list);
+    }
+}
