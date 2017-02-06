d25cc8d42d4c2b5c24215c5648f911c6513af81e
liuyang <494821083@qq.com>
2017-02-06 20:10:54 +0800

    volidate

---
+++ springbk-app/src/main/java/com/blueknight/vo/Volatile.java
@@ -0,0 +1,37 @@
+package com.blueknight.vo;
+
+import java.util.concurrent.TimeUnit;
+
+public class Volatile {
+
+    private static boolean bChanged;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread() {
+            @Override
+            public void run() {
+                for (; ; ) {
+//                    String currentThread = Thread.currentThread().getName();
+                    System.out.println(bChanged);
+                    if (bChanged) {
+                        System.out.println("!=");
+                        break;
+                    }
+                }
+            }
+        }.start();
+
+        TimeUnit.MILLISECONDS.sleep(10L);
+
+        new Thread() {
+            @Override
+            public void run() {
+                bChanged = true;
+                System.out.println(1);
+            }
+        }.start();
+
+        TimeUnit.MILLISECONDS.sleep(10L);
+        System.out.println("main thread ->> " + bChanged);
+    }
+}
